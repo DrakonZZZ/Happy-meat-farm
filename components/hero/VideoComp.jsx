4ff74f9e2450e4f4886668305dbd039acc4d4e78
Drakonz <darkprince.selvan@gmail.com>
2023-10-19 01:48:34 +0530

    adding small details

--- components/hero/VideoComp.jsx
+++ components/hero/VideoComp.jsx
@@ -1,24 +1,37 @@
+import Image from 'next/image';
+import fieldView from '../../public/images/view.webp';
 import { BsFillArrowRightCircleFill } from 'react-icons/bs';
 import { PiPottedPlant, PiFlask, PiTruckDuotone } from 'react-icons/pi';
 
 const VideoComp = () => {
   return (
     <div className="flex w-full flex-col-reverse md:flex-row gap-6 mt-6">
-      <div className="bg-[#112421] flex flex-col md:flex-row justify-evenly rounded-2xl p-4">
-        <div className="p-2 w-full md:w-3/4">
-          <h3 className="text-3xl md:text-6xl font-semibold text-[#91A884] shadow-sm mb-4">
-            A farm you can trust
-          </h3>
-          <p className="opacity-75">
-            Our perspective doesn't categorize problems as obstacles, but rather
-            as opportunities to find solutions. At Happy Meat Farms, our
-            unwavering commitment is to produce the healthiest and most
-            ethically raised meat products on the market. In an industry where
-            cutting corners for profit is common, we stand apart by prioritizing
-            innovation and reinventing farming practices. We invite you to be
-            part of the journey to experience the future today, where quality
-            and ethics converge.
-          </p>
+      <div className="bg-[#112421] flex flex-col md:flex-row rounded-2xl p-4 gap-4">
+        <div className="w-full flex flex-col justify-evenly md:pr-4">
+          <div>
+            <h3 className="text-3xl md:text-6xl font-semibold text-[#91A884] shadow-sm mb-4">
+              A farm you can trust
+            </h3>
+            <p className="opacity-75">
+              We don't see problems as problems, we see problems as future
+              solutions. Happy Meat Farms is dedicated to making the healthiest,
+              most humane meat based products on the market. Where others cut
+              corners to make a profit, we strive to innovate and revolutionize
+              the farming process. Experience tomorrow's future today
+            </p>
+          </div>
+
+          <div className="relative w-full h-[200px] md:h-full mt-8">
+            <Image
+              src={fieldView}
+              alt="about-us-image"
+              fill={true}
+              style={{ objectFit: 'cover', borderRadius: '12px' }}
+            />
+            <span className="arrow absolute flex items-center gap-3 bottom-1 right-2 bg-[#112421] text-[#91A884] py-2 px-3 rounded-lg text-sm font-bold cursor-pointer hover:text-[#112421] hover:bg-[#91A884] transition">
+              About Us
+            </span>
+          </div>
         </div>
 
         <div className="flex flex-row md:flex-col gap-2 w-full md:w-1/4">
@@ -30,21 +43,27 @@ const VideoComp = () => {
               size={80}
               className="absolute right-[-20px] text-[#264944] opacity-25"
             />
-            <p className="text-sm md:text-lg">Organic Products</p>
+            <p className="text-sm md:text-xl md:font-semibold opacity-75">
+              Organic Products
+            </p>
           </span>
           <span className="relative w-full h-full flex items-center bg-[#18332F] p-2 rounded-lg overflow-clip">
             <PiFlask
               size={80}
               className="absolute right-[-20px] text-[#264944] opacity-25"
             />
-            <p className="text-sm md:text-lg">Research & Development</p>
+            <p className="text-sm md:text-lg md:font-semibold opacity-75">
+              Research & Development
+            </p>
           </span>
           <span className="relative w-full h-full flex items-center bg-[#18332F] p-2 rounded-lg overflow-clip">
             <PiTruckDuotone
               size={80}
               className="absolute right-[-20px] text-[#264944] opacity-25"
             />
-            <p className="text-sm md:text-lg">Logistics & Equipment</p>
+            <p className="text-sm md:text-lg md:font-semibold opacity-75">
+              Logistics & Equipment
+            </p>
           </span>
         </div>
       </div>
---
+++ data/newsdata.js
@@ -0,0 +1,23 @@
+export const news = [
+  {
+    id: 1,
+    title: 'HMF supports the community',
+    content: `As many of you know, HMF inc. has begun supporting the online community by sponsoring up and coming creators. We think this is a great way to help artists grow and meet new collaborators.`,
+    date: 'July 09 2021',
+    src: 'news/planting.webp',
+  },
+  {
+    id: 2,
+    title: 'R&D department expanded',
+    content: `Since 2014 HMF Inc. has put a heavy emphasis on their R&D department, but we're happy to announce that we'll be expanding the department into 2 new facilities!`,
+    date: 'March 15, 2020',
+    src: 'news/cows.webp',
+  },
+  {
+    id: 3,
+    title: 'HMF wins landmark libel case',
+    content: `As many of you know, HMF inc. has begun supporting the online community by sponsoring up and coming creators. We think this is a great way to help artists grow and meet new collaborators.`,
+    date: 'January 03, 2019',
+    src: 'news/justice.webp',
+  },
+];
